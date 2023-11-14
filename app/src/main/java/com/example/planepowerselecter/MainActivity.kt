65b02bce21910a8f123052c60dc95d6a634233f5
kimjaeuk <kju4133@naver.com>
2023-11-14 14:51:50 +0900

    에러 메세지 구현 성공! 코틀린 내에서 if문 사용 시 반드시 중괄호 {}를 사용할 것!

--- app/src/main/java/com/example/planepowerselecter/MainActivity.kt
+++ app/src/main/java/com/example/planepowerselecter/MainActivity.kt
@@ -26,10 +26,13 @@ class MainActivity : AppCompatActivity() {
             var input: String = findViewById<TextView>(R.id.aircraft_weight_value).text.toString()
             var weight: Int
 
+            //입력이 없을 경우
             if (input == ""||input == "0") {
                 Toast.makeText(getApplicationContext(), "모터 중량이 입력되지 않았습니다", Toast.LENGTH_LONG).show()
             }
-            else {//입력이 있을 경우
+
+            //입력이 있을 경우
+            else {
                 weight = input.toInt()
                 intent.putExtra("aircraftWeight", weight)
                 startActivity(intent)
