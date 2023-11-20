84e3b115d913cf0aba2d43f86db0455b8543845a
kimjaeuk <kju4133@naver.com>
2023-11-20 12:44:43 +0900

    정렬 알고리즘 구현 전,  리스트에 모터 목록을 나열해주는 Add_Info() 메소드 생성

--- app/src/main/java/com/example/planepowerselecter/SubActivity.kt
+++ app/src/main/java/com/example/planepowerselecter/SubActivity.kt
@@ -18,5 +18,35 @@ class SubActivity : AppCompatActivity() {
         var requiredPower = findViewById<TextView>(R.id.required_motor_power)
         requiredPower.text = round(((weight * 80) / 453.6)).toString() + " (watt)"
 
+        Add_Info()
+    }
+
+    fun Add_Info(){
+
+        //여기서 제공되는 (2차원)배열은 정렬이 끝난 배열이라 가정함
+        var arrayString = arrayOf(
+            arrayOf("하나","둘"),
+            arrayOf("셋","넷"),
+            arrayOf("다섯","여섯"),
+            arrayOf("일곱","여덟"),)
+
+        var infoList1 = Array<String>(arrayString.count(),{""})
+        for(i: Int in 0..3){
+            for(j: Int in 0..1){
+                infoList1[i] = infoList1[i].plus(arrayString[i][j])
+                infoList1[i] = infoList1[i].plus(" ")
+            }
+        }
+
+        var infoList0:String = ""
+        for(i: Int in 0..3){
+            infoList0 = infoList0.plus(infoList1[i])
+            infoList0 = infoList0.plus("\n")
+        }
+
+        var MotorList = findViewById<TextView>(R.id.MotorList)
+        MotorList.text = infoList0;
+
+
     }
 }
